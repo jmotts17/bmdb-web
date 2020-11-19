074024311a8df9cf224ae8384bae1df20d170fd0
Josh Motta <joshua.a.motta@gmail.com>
2020-11-18 21:11:19 -0500

    Added recalculate method

--- src/main/java/com/bmdb/db/MovieCollectionRepo.java
+++ src/main/java/com/bmdb/db/MovieCollectionRepo.java
@@ -1,9 +1,13 @@
 package com.bmdb.db;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bmdb.business.MovieCollection;
 
 public interface MovieCollectionRepo extends JpaRepository<MovieCollection, Integer> {
 
+	public List<MovieCollection> findByUserId(int id);
+	
 }
--- src/main/java/com/bmdb/web/MovieCollectionController.java
+++ src/main/java/com/bmdb/web/MovieCollectionController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.bmdb.business.MovieCollection;
+import com.bmdb.business.User;
 import com.bmdb.db.MovieCollectionRepo;
+import com.bmdb.db.UserRepo;
 
 @CrossOrigin
 @RestController
@@ -16,6 +18,8 @@ public class MovieCollectionController {
 
 	@Autowired
 	private MovieCollectionRepo movieCollectionRepo;
+	@Autowired
+	private UserRepo userRepo;
 	
 	// Get all movieCollections
 	@GetMapping("/")
@@ -33,6 +37,7 @@ public class MovieCollectionController {
 	@PostMapping("/")
 	public MovieCollection addMovieCollection(@RequestBody MovieCollection m) {
 		m = movieCollectionRepo.save(m);
+		recalculateCollectionValue(m);
 		return m;
 	}
 	
@@ -40,21 +45,37 @@ public class MovieCollectionController {
 	@PutMapping("/")
 	public MovieCollection updateMovieCollection(@RequestBody MovieCollection m) {
 		m = movieCollectionRepo.save(m);
+		recalculateCollectionValue(m);
 		return m;
 	}
 	
 	// Delete a movieCollection by id
 	@DeleteMapping("{id}")
 	public MovieCollection deleteMovieCollection(@PathVariable int id) {
-		// Optional type will wrap a movieCollection
 		Optional<MovieCollection> m = movieCollectionRepo.findById(id);
-		// isPresent() will return true if a movieCollection was found
+
 		if (m.isPresent()) {
 			movieCollectionRepo.deleteById(id);
+			recalculateCollectionValue(m.get());
 		} else {
 			System.out.println("Error - movieCollection not found for id " + id);
 		}
 		return m.get();
+	}
+		
+	public void recalculateCollectionValue(MovieCollection m) {
+		// get all movie collections for this user
+		// loop through them and sum a new total
+		double newTotal = 0.0;
+		List<MovieCollection> mcs = movieCollectionRepo.findByUserId(m.getUser().getId());
+		
+		for(MovieCollection mc : mcs) {
+			newTotal += mc.getPurchasePrice();
+		}
+		
+		User u = m.getUser();
+		u.setCollectionValue(newTotal);
+		userRepo.save(u);
 	}
 	
 }
