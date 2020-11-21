ef7943df82402a642bf2ca2e4c900d5865a82db6
Josh Motta <joshua.a.motta@gmail.com>
2020-11-21 11:21:44 -0500

    Added User Login

--- src/main/java/com/bmdb/db/UserRepo.java
+++ src/main/java/com/bmdb/db/UserRepo.java
@@ -1,9 +1,14 @@
 package com.bmdb.db;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bmdb.business.User;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
-
+	
+	// Find a User by username and password
+	Optional<User> findByUsernameAndPassword(String username, String password);
+	
 }
